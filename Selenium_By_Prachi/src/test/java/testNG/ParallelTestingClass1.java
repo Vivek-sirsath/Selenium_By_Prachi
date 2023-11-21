243b6ddffb2a2fcea04671c8d56eb82358350e79
vivek <vivek.sirsath1@gmail.com>
2023-11-21 18:58:02 +0530

    @Test tag alignment issue

--- Selenium_By_Prachi/src/test/java/testNG/ParallelTestingClass1.java
+++ Selenium_By_Prachi/src/test/java/testNG/ParallelTestingClass1.java
@@ -10,43 +10,43 @@ import org.testng.annotations.Test;
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class ParallelTestingClass1 {
-	
+
 	WebDriver driver;
-	
+
 	@Test
 	public void verifyTitle() throws Exception {
-		
+
 		// Lauch chrome browser
 		WebDriverManager.chromedriver().setup();
 		driver = new ChromeDriver();
-		
+
 		driver.get("https://www.saucedemo.com/");
-		
+
 		System.out.println("Title of WebPage: " + driver.getTitle());
 		Assert.assertEquals(driver.getTitle(), "Swag Labs");
-		
-		driver.quit();	
-		
+
+		driver.quit();
+
 		Thread.sleep(3000);
 	}
-	
-       @Test
+
+	@Test
 	public void verifyLogo() throws Exception {
-		
+
 		// Lauch chrome browser
 		WebDriverManager.chromedriver().setup();
 		driver = new ChromeDriver();
-		
+
 		driver.get("https://www.saucedemo.com/");
-		
+
 		WebElement logo = driver.findElement(By.xpath("//div[@class='login_logo']"));
-		
+
 		Thread.sleep(3000);
-		
+
 		Assert.assertTrue(logo.isDisplayed());
-		
+
 		driver.quit();
-		
+
 	}
 
 }
