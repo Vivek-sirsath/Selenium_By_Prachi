96bdb893fb88840b43cdb2ed0de885e38a458f00
vivek <vivek.sirsath1@gmail.com>
2023-12-16 15:32:21 +0530

    Parameterized XPath program added

---
+++ Selenium_By_Prachi/src/test/java/parameterizedXPath/ParameterizedXpath.java
@@ -0,0 +1,44 @@
+package parameterizedXPath;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class ParameterizedXpath {
+
+	public static void main(String[] args) throws InterruptedException {
+		System.setProperty("webdriver.chrome.driver", "E:\\EDUCATIONAL\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://www.phptravels.net/flights");
+		Thread.sleep(15000);
+		/*
+		 * // Below way we are repeating the code, just values in xpath arre changing
+		 * 
+		 * driver.findElement(By.xpath("//a[text()='Hotels']")).click();
+		 * Thread.sleep(5000);
+		 * 
+		 * driver.findElement(By.xpath("//a[text()='Flights']")).click();
+		 * Thread.sleep(5000);
+		 * 
+		 * driver.findElement(By.xpath("//a[text()='Cars']")).click();
+		 * Thread.sleep(5000);
+		 */
+		
+		commonXPath(driver, "Tours"); // calling static method
+		commonXPath(driver, "Hotels");
+		commonXPath(driver, "Flights");
+		
+		driver.close();
+	}
+	
+	// We need to declare the method static 
+	// Because in static main method we can call static method directly by its Name
+	public static void commonXPath(WebDriver driver, String value) throws InterruptedException {
+		WebElement element = driver.findElement(By.xpath("//a[text()='"+value+"']"));
+		Thread.sleep(5000);
+		element.click();
+		
+	}
+}
