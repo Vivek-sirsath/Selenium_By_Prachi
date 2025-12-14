0481cfd490d5d430e6f44632052961d96c59d8d2
vivek <vivek.sirsath1@gmail.com>
2025-12-14 21:26:55 +0530

    ElementNotInteractableException.java modified for comments.

--- Selenium_By_Prachi/src/test/java/seleniumExceptionsQAFox/ElementNotInteractableException.java
+++ Selenium_By_Prachi/src/test/java/seleniumExceptionsQAFox/ElementNotInteractableException.java
@@ -10,12 +10,13 @@ public class ElementNotInteractableException {
 
 	public static void main(String[] args) {
 		
-		WebDriverManager.chromedriver().setup();
+		
 //		System.setProperty("webdriver.chrome.driver", "E:\\Educational\\chromedriver.exe");
 //		Exception in thread "main" org.openqa.selenium.SessionNotCreatedException: Could not start a new session. 
 //		Response code 500. 
 //		Message: session not created: This version of ChromeDriver only supports Chrome version 131
 		
+		WebDriverManager.chromedriver().setup();
 		WebDriver driver = new ChromeDriver();
 		driver.manage().window().maximize();
 		
@@ -28,11 +29,13 @@ public class ElementNotInteractableException {
 		
 //		Case: 2 - SearchBox 
 //		driver.get("https://omayo.blogspot.com");
+		// find text field (Search This Blog) 
 //		driver.findElement(By.className("gsc-input")).sendKeys("Vivek");
 		
 //		Case: 3 - Hidden Button
 //		'Hidden Button' attribute is there but, Actually button is not visible.
 		driver.get("https://omayo.blogspot.com");
+		// Click on Disable-Enable Button - (My Button)
 		driver.findElement(By.id("hbutton")).click();
 		
 	}
